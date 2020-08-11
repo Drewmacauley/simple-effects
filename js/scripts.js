04f555b01d5fedecf463696dc3ab106f27b0fc07
Alma <a.q.frankenstein@gmail.com>
2020-08-11 10:18:12 -0700

    add some classes and ids
    Co-authored by: Drew Macauley <macauleyandy03@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,21 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $("p").click(function() {
-    $("#walrus-showing").show();
-    $("#walrus-showing").hide();
+    $(".walrus-showing").show();
+    $(".walrus-showing").hide();
   });
 });
 
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+});
+
+ (document).ready(function)() {
+fadeIn(".clickable").click(function() {
+  ("#walrus-showing").toggle();
+  ("#walrus-hidden").toggle()
   });
 });
